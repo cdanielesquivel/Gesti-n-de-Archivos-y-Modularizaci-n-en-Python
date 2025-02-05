2bc0670fcd32ced8f0526e8162714d9c16303864
cdanielesquivel <cdanielesquivel@gmail.com>
2025-02-04 21:00:48 -0600

    sesión5

--- analisis_datos/estadisticas.py
+++ analisis_datos/estadisticas.py
@@ -1,1 +1,28 @@
 # estadisticas.py
+def media(datos):
+    """Esta función calcula la media aritmétrica de una lista con valores numéricos
+
+    Args:
+        datos (Lista): Lista de números
+
+    Returns:
+        Float: flotante de la media aritmétrica
+    """
+    return sum(datos) / len(datos)
+
+def calcular_mediana(datos):
+    """_summary_
+
+    Args:
+        datos (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    datos_sorted = sorted(datos)
+    n = len(datos)
+    mid = n // 2
+    if n % 2 == 0:
+        return (datos_sorted[mid - 1] + datos_sorted[mid]) / 2.0
+    else:
+        return datos_sorted[mid]
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 # Proyecto: [Nombre del Proyecto]
-# Estudiante: [Nombre del Estudiante]
-# Fecha de Inicio: [dd/mm/aaaa]
-# Fecha de Entrega: [dd/mm/aaaa]
+# Estudiante: Carlos Daniel Esquivel Bolaños
+# Fecha de Inicio: 24/02/2025
+# Fecha de Entrega: 24/02/2025
 # Descripción: Este archivo contiene el punto de entrada principal del proyecto.
 # Recuerda incluir tu nombre completo, la fecha en la que iniciaste el proyecto y la fecha estimada de entrega.
 # Esto ayuda a mantener un registro claro del trabajo realizado.
+
+# from analisis_datos.estadisticas import media,calcular_mediana
+
+from analisis_datos import *
+
+lista_compras = generar_lista_de_compras()
+print(lista_compras)
+
+lista_parametro = [5,3,1,2,4]
+print('Media: ', media(lista_parametro))
+print('Mediana: ', calcular_mediana(lista_parametro))
